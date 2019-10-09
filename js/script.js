faf40902feda24b1d5107daf3b313babed9ef007
serennis <32954791+serennis@users.noreply.github.com>
2019-10-09 15:09:44 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 console. log ("Welcome to my portfolio!")
+function menuToggle() {
+  var x = document.getElementById("mynavtoggle");
+  if (x.className === "navtoggle")
+  else {
+    x.className = "navtoggle";
+  }
+
