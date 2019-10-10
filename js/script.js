12ac49f3265bab5165a6a34abbf60051da814452
serennis <32954791+serennis@users.noreply.github.com>
2019-10-10 13:38:32 +0200

    new changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console. log ("Welcome to my portfolio!")
+// console. log ("Welcome to my portfolio!")
+// function menuToggle() {
+  // var x = document.getElementById("mynavtoggle");
+  // if (x.className === "navtoggle")
+  // else {
+  //  x.className = "navtoggle";
+  //}
+
+  // Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon
+  function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
